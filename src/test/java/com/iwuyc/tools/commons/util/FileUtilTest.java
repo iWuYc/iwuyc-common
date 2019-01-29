c593f1e515f2103a857596eb2a5eb85a08df99d0
Neil <iwuyc@foxmail.com>
2019-01-29 11:35:41 +0800

    新增：文件工具类增加获取文件绝对路径的方法。

--- src/test/java/com/iwuyc/tools/commons/util/FileUtilTest.java
+++ src/test/java/com/iwuyc/tools/commons/util/FileUtilTest.java
@@ -5,27 +5,34 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FileUtilTest {
+
     private String path = "/Users/iwuyc/Downloads/sbt copy.tar";
 
     @Test
-    public void test() {
+    public void test(){
         assertFalse(FileUtil.safeDelete(path));
 
         URL testPropertiesUrl = FileUtilTest.class.getResource("/");
-        String filePath = testPropertiesUrl.getFile()+"test.properties";
+        String filePath = testPropertiesUrl.getFile() + "test.properties";
         File file = new File(filePath);
-        try {
+        try{
             file.createNewFile();
-        } catch (IOException e) {
+        }catch(IOException e){
             e.printStackTrace();
         }
         assertTrue(FileUtil.safeDelete(filePath));
 
     }
 
+    @Test
+    public void absoluteLocation(){
+        Optional<String> result = FileUtil.absoluteLocation("classpath:/test.properties");
+        System.out.println(result);
+    }
 }
