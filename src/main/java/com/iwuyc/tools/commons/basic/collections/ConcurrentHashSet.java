593975c04d38b41f24924ece5b23bbd5ec498d43
Neil <wuych@ibumobile.com>
2019-12-06 15:55:57 +0800

    新增：线程安全的hashSet

---
+++ src/main/java/com/iwuyc/tools/commons/basic/collections/ConcurrentHashSet.java
@@ -0,0 +1,20 @@
+package com.iwuyc.tools.commons.basic.collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 线程安全的hash set实现
+ * @author 吴宇春
+ */
+public class ConcurrentHashSet<T> extends EmbellishSet<T> {
+    public ConcurrentHashSet() {
+        super(new ConcurrentHashMap<T, Boolean>());
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public boolean add(T o) {
+        Object previousVal = ((ConcurrentHashMap) this.container).putIfAbsent(o, Boolean.TRUE);
+        return previousVal == null;
+    }
+}
