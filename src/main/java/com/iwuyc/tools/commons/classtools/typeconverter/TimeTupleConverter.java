6ba2a4e0c6fab3ec6f25d42f93e864db5469aea5
吴宇春 <wuyuchun@rainbowcn.com>
2021-08-10 18:22:53 +0800

    feat(v0.1.2): 添加测试用例
    添加测试用例，删除依赖。

--- src/main/java/com/iwuyc/tools/commons/classtools/typeconverter/TimeTupleConverter.java
+++ src/main/java/com/iwuyc/tools/commons/classtools/typeconverter/TimeTupleConverter.java
@@ -1,24 +1,16 @@
 package com.iwuyc.tools.commons.classtools.typeconverter;
 
-import com.iwuyc.tools.beans.converter.string.StringConverter;
 import com.iwuyc.tools.commons.basic.type.TimeTuple;
-import com.iwuyc.tools.commons.util.collection.Sets;
 
-import java.util.Set;
-
-public class TimeTupleConverter extends StringConverter<TimeTuple> {
+public class TimeTupleConverter extends AbstractStringConverter<TimeTuple> {
     @Override
-    public TimeTuple convert(String source, Class<? extends TimeTuple> targetClass) {
-        return String2TimeTupleConverter.converter(source, targetClass);
+    protected TimeTuple converterData(String source, Class<? extends TimeTuple> targetType) {
+        return String2TimeTupleConverter.converter(source, targetType);
     }
 
     @Override
-    public boolean support(Class<? extends TimeTuple> targetClass) {
+    protected boolean isSupport(Class<?> targetClass) {
         return TimeTuple.class.isAssignableFrom(targetClass);
     }
 
-    @Override
-    protected Set<Class<? extends TimeTuple>> getSupportClass() {
-        return Sets.asSet(TimeTuple.class);
-    }
 }
