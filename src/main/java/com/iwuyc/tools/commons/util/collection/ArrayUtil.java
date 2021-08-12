8e6819f7b014fe5a0e7e738b1e820bf77d51f30b
Neil <iwuyc@foxmail.com>
2021-08-12 18:05:37 +0800

    feat(v0.1.2): ArrayUtil新增基础类型数组的判断
    ArrayUtil新增基础类型数组的判断
    
    [skip ci]
    
    Signed-off-by: Neil <iwuyc@foxmail.com>

--- src/main/java/com/iwuyc/tools/commons/util/collection/ArrayUtil.java
+++ src/main/java/com/iwuyc/tools/commons/util/collection/ArrayUtil.java
@@ -14,6 +14,14 @@ public abstract class ArrayUtil {
         return !isEmpty(arrays);
     }
 
+    public static boolean isEmpty(long[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(long[] arrays) {
+        return !isEmpty(arrays);
+    }
+
     public static boolean isEmpty(int[] arrays) {
         return null == arrays || 0 == arrays.length;
     }
@@ -22,7 +30,58 @@ public abstract class ArrayUtil {
         return !isEmpty(arrays);
     }
 
+    public static boolean isEmpty(short[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(short[] arrays) {
+        return !isEmpty(arrays);
+    }
+
+    public static boolean isEmpty(byte[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(byte[] arrays) {
+        return !isEmpty(arrays);
+    }
+
+    public static boolean isEmpty(float[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(float[] arrays) {
+        return !isEmpty(arrays);
+    }
+
+    public static boolean isEmpty(double[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(double[] arrays) {
+        return !isEmpty(arrays);
+    }
+
+    public static boolean isEmpty(char[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(char[] arrays) {
+        return !isEmpty(arrays);
+    }
+
+    public static boolean isEmpty(boolean[] arrays) {
+        return null == arrays || 0 == arrays.length;
+    }
+
+    public static boolean isNotEmpty(boolean[] arrays) {
+        return !isEmpty(arrays);
+    }
+
     public static <T> int arrayLength(T[] arrays) {
-        return null == arrays ? 0 : arrays.length;
+        return isEmpty(arrays) ? 0 : arrays.length;
+    }
+    public static <T> int arrayLength(long[] arrays) {
+        return isEmpty(arrays) ? 0 : arrays.length;
     }
 }
