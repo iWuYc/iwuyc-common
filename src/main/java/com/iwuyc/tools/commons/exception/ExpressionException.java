5414cce50894147f8d7c0e8f185274c05dd38ffa
Neil <iwuyc@foxmail.com>
2022-01-04 11:10:15 +0800

    feat(v0.1.2): Range.java增加测试用例以及说明文档
    Range.java增加测试用例以及说明文档
    
    [skip ci]
    
    Signed-off-by: Neil <iwuyc@foxmail.com>

---
+++ src/main/java/com/iwuyc/tools/commons/exception/ExpressionException.java
@@ -0,0 +1,11 @@
+package com.iwuyc.tools.commons.exception;
+
+public class ExpressionException extends RuntimeException {
+    public ExpressionException(String message) {
+        super(message);
+    }
+
+    public ExpressionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
